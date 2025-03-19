fa79afca9c655846dde1cad1151fb8efbb5d320d
Gandarillas <lizeth.gandarillas@dharbor.com>
2025-03-19 01:20:13 -0400

    correct factory conditional

--- gilded-rose-master/Java/src/main/java/com/gildedrose/factory/ItemUpdaterFactory.java
+++ gilded-rose-master/Java/src/main/java/com/gildedrose/factory/ItemUpdaterFactory.java
@@ -3,9 +3,10 @@ package com.gildedrose.factory;
 import com.gildedrose.Item;
 import com.gildedrose.updater.*;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 /**
  * @author Lizeth Gandarillas
@@ -17,17 +18,21 @@ public class ItemUpdaterFactory {
     private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
     private static final String CONJURED = "Conjured";
 
-    private static final Map<String, ItemUpdater> strategies = new HashMap<>();
+    private final Map<Predicate<Item>, Function<Item, ItemUpdater>> strategies = new LinkedHashMap<>();
 
-    static {
-        strategies.put(AGED_BRIE, new AgedBrieUpdater());
-        strategies.put(BACKSTAGE_PASSES, new BackstagePassUpdater());
-        strategies.put(SULFURAS, new SulfurasUpdater());
-
-        strategies.put(CONJURED, new ConjuredItemUpdater()); // Nuevo tipo agregado
+    public ItemUpdaterFactory() {
+        strategies.put(item -> item.name.equals(AGED_BRIE), item -> new AgedBrieUpdater());
+        strategies.put(item -> item.name.equals(BACKSTAGE_PASSES), item -> new BackstagePassUpdater());
+        strategies.put(item -> item.name.equals(SULFURAS), item -> new SulfurasUpdater());
+        strategies.put(item -> item.name.contains(CONJURED), item -> new ConjuredItemUpdater());
     }
 
-    public static ItemUpdater createUpdater(Item item) {
-        return strategies.getOrDefault(item.name, new StandardItemUpdater());
+    public ItemUpdater createUpdater(Item item) {
+        for (Map.Entry<Predicate<Item>, Function<Item, ItemUpdater>> entry : strategies.entrySet()) {
+            if (entry.getKey().test(item)) {
+                return entry.getValue().apply(item);
+            }
+        }
+        return new StandardItemUpdater();
     }
 }
